09a314e2b65190faaa59d647a79fdefc16986559
JosepharDev <josephardev@gmail.com>
2023-06-18 09:23:04 -0700

    print comb 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Start point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
