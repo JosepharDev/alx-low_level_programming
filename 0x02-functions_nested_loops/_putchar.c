f0abfc4d5703331423ab4e96fff9be8cb9dd2164
JosepharDev <josephardev@gmail.com>
2023-06-21 04:57:23 -0700

    putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Start point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	return (0);
+}
