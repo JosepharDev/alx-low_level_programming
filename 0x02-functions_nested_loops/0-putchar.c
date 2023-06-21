c5dfc74768d64b0e0d3affdd99d3ce3ade112089
JosepharDev <josephardev@gmail.com>
2023-06-21 04:24:49 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,12 @@
  */
 void _putchar(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		write(1, *str, 1);
-		str++;
+		write(1, &str[i], 1);
+		i++;
 	}
 	write(1, "\n", 1);
 }
