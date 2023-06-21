3c3dbf5984e4c42f9899dd07214f8bdbba805582
JosepharDev <josephardev@gmail.com>
2023-06-21 06:18:21 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_alpha - function to print aphabet in lowecase
+ *
+ * return: void
+ */
+void print_alpha(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar(c);
+}
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		print_alphabet_x10();
+		i++;
+	}
+}
