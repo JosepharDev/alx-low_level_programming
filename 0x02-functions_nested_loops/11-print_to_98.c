6561e3596fe3ce0199b974e1371c7dd1074c4c21
JosepharDev <josephardev@gmail.com>
2023-06-21 13:56:02 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,31 @@ void print_to_98(int n)
 			n++;
 		}
 	}
-	else if (n >= 0)
+	if (n >= 0)
 	{
 		while (n <= 97)
 		{
+			if (n == 98)
+			{
+				pintf("%d\n", n);
+				return;
+			}
 			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n == 97)
+
+	if (n >= 98)
 	{
-		printf("%d\n", ++n);
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+				pintf("%d\n", n);
+				return;
+			}
+			printf("%d, ", n);
+			n++;
+		}
 	}
 }
