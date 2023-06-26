8ed4f99ca4b5f955dd5b4b4558ee17bae706621b
JosepharDev <josephardev@gmail.com>
2023-06-26 15:28:17 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string- function to rev string in place
+ * @s: string passed as parameter to rev
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int start = 0;
+	int end = 0;
+	char tmp;
+
+	while (s[len])
+		len++;
+
+	end = len - 1;
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+	write(1, "\n", 1);
+}
