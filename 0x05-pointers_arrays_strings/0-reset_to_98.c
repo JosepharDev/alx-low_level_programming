ec453ae6abdec149e000bf97dad1423efc31a726
JosepharDev <josephardev@gmail.com>
2023-06-26 14:02:21 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function reset value of varaible passed as address
+ * @n: varaible passed as refference to  reset it's value
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
