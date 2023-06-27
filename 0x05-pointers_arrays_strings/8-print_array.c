707b0a725c1bc40ad2b2be55d7398136cf8154bd
JosepharDev <josephardev@gmail.com>
2023-06-26 17:05:03 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,9 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		if (i + 1 == n)
-			printf("%d\n", a[i++]);
+			printf("%d", a[i++]);
 		else
 			printf("%d, ", a[i++]);
 	}
+	printf("\n");
 }
